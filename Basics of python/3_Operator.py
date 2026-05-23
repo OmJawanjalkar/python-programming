93b6a5f36fe1adf3ef4783673bc7873cd206e292
Om Jawanjalkar <omjawanjalkar@gmail.com>
2026-05-23 09:32:22 +0530

    Basics of python

---
+++ Basics of python/3_Operator.py
@@ -0,0 +1,45 @@
+# a = 10
+b = 20
+
+#  Opratpors in Python
+# 1. Arithmetic Operators
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a ** b)
+
+
+#  2.  Comparision Operator 
+
+# print(a==b)
+# print(a != b)
+# print(a>b)
+# print(a<b)
+# print(a <= b)
+# print (a >= b)
+
+# 3. Assignment Operator
+# a = 25
+
+# a += 5
+# a -= 5
+# a *= 5
+# a /= 5
+# print(a)
+
+
+# 4. Membership operator
+# name = "Om Jawanjalkar"
+# print ( "Om" in name )
+# print("Om Jawanjalkar" not in name)
+
+
+# 6. Identity Operators
+
+# a = 10
+# b = 10
+
+# print(a is b)
+# print(a is not b)
